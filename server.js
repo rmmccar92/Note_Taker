394ca355fec17dd26e2bed6c75747604f71d3205
Ryan McCarthy <rmmcc92@aol.com>
2021-08-18 13:56:11 -0400

    didn't have 'port' capitalized which is obviously going to cause issues

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./db/db.json');
 const notePath = require('./routes/notes')
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
